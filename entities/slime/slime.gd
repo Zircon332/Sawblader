52cd880c4acdd14fccc4f9a7817e892f6b0ff948
Zircon <zircon332@gmail.com>
2023-03-25 15:31:44 +0800

    added slime spawner

--- entities/slime/slime.gd
+++ entities/slime/slime.gd
@@ -1,16 +1,22 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(int) var speed := 100
+export(float) var spread := 30
+
+var direction := Vector2.UP
+var size := 3
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta) -> void:
+	position += delta * speed * direction
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func spawn() -> void:
+	var center_offset := 50
+	var target := Vector2(
+		rand_range(center_offset, get_viewport_rect().size.x - center_offset),
+		rand_range(center_offset, get_viewport_rect().size.y - center_offset)
+	)
+	
+	direction = position.direction_to(target)
---
+++ spawner/spawner.gd
@@ -0,0 +1,29 @@
+extends Path2D
+
+
+const entityScene := preload("res://entities/slime/slime.tscn")
+
+onready var context := get_parent()
+
+onready var _rng := RandomNumberGenerator.new()
+onready var _spawn_point := $SpawnPoint
+
+
+func spawn_pack(pack_size:int = 4) -> void:
+	for _i in range(pack_size):
+		_spawn_point.unit_offset = _rng.randf()
+		spawn(_offset_position(_spawn_point.global_position))
+
+
+func spawn(pos: Vector2) -> void:
+	var entity := entityScene.instance()
+	entity.global_position = pos
+	context.add_child(entity)
+	entity.spawn()
+
+
+func _offset_position(pos: Vector2) -> Vector2:
+	var angle := _rng.randf_range(-PI, PI)
+	var direction := Vector2.RIGHT.rotated(angle)
+	var vector := direction * randf()
+	return pos + vector
