c8dc6d1c696c1a3b142c02edd14a802094e05b6c
Zircon <zircon332@gmail.com>
2023-03-25 15:35:28 +0800

    despawn enemy when off-screen

--- entities/slime/slime.gd
+++ entities/slime/slime.gd
@@ -20,3 +20,11 @@ func spawn() -> void:
 	)
 	
 	direction = position.direction_to(target)
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	pass
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	queue_free()
